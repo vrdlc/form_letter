fd0f4d2e7c8b4182ee08f9e73ab37f75bfc0d2cb
Afton Downey and Ramon de la Cruz <aaike001@gmail.com>
2016-01-12 13:15:04 -0800

    add .toUpperCase

--- js/scripts_uppercase.js
+++ js/scripts_uppercase.js
@@ -3,8 +3,10 @@ $(document).ready(function() {
   $("#blanks form").submit(function(event) {
   var somethingInput = $("input#something").val();
 
-  $(".something").text(somethingInput);
+  $(".something").text(somethingInput.toUpperCase());
+  // $("p").toLocalUpperCase(somethingInput);
 
+  //somethingInput.toUpperCase();
   event.preventDefault();
   });
 

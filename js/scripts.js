317fed1179d8ce179ef3f46e8761363d2f5c3933
Afton Downey and Ramon de la Cruz <virgilioramon.delacruz@gmail.com>
2016-01-12 11:10:17 -0800

    add template pages and set up initial form and letter.

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+  $("#blanks form").submit(function() {
+    $(".name1").append("blah blah");
+    $(".name2").append("blah blah");
+    $(".email1").append("blah blah");
+  });
+
+});
